ea5da1614199970355f164d08c8d8ee4bcecbece
EHoyle <erich.hoyle1@gmail.com>
2016-04-03 15:39:07 -0400

    Added code to check for text in inputs

--- js/main.js
+++ js/main.js
@@ -3,8 +3,24 @@ $(document).ready(function() {
 	$('#erich-form').submit(function(e) {
 
 		e.preventDefault();
-		
-		console.log("HELLO");
+
+		var hasError = false;
+
+		var $forminputs = $("form#erich-form input");
+
+		for (i = 0; i < $forminputs.length; i++){
+
+			var forminput = $forminputs.eq(i).val();
+			if (forminput.length <= 0) {
+				hasError = true;
+			}
+		}
+
+		if(hasError) {
+			return false;
+		} else {
+			return true;
+		}
 	});
 
 });
